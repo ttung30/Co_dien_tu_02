9f1f8453805ca27eeaf720f426e1c77af1e35bff
iamtherealcheng <100274771+iamtherealcheng@users.noreply.github.com>
2022-05-11 22:53:30 +0700

    Update bai1_3.c

--- code/bai1_3.c
+++ code/bai1_3.c
@@ -7,15 +7,15 @@
 unsigned int dem=0,n=0;
 void main(void)
 {
-	ADCON1=0x0f;
-	TRISB=0b00001111;
-	TRISD=0;
+	ADCON1=0x0f; // thiết lập vào ra số
+	TRISB=0b00001111;// chọn chiều vào cho 4 đầu tiên trên port B
+	TRISD=0;// thiết lập chiều vào cho port D
 	while(1){
 	while(PB);
 	while(!PB);
 	dem++;
 	n=dem%16;
-	PORTD=n;
+	PORTD=n;// xuất giá trị ra PortD
 
 }
 }
